e7c90b56fd42ed0691670e63fab5978b6b2b2880
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-06-24 19:46:41 +0530

    🟢 Sqrt(x) [Easy] - java

---
+++ 69-sqrtx/solution.java
@@ -0,0 +1,36 @@
+/*
+ * LeetCode #69 - Sqrt(x)
+ * Difficulty : Easy
+ * Language   : java
+ * Runtime    : 1 ms
+ * Memory     : 42.52 MB
+ * URL        : https://leetcode.com/problems/sqrtx/
+ */
+
+class Solution {
+    public int mySqrt(int x) {
+
+        if (x < 2) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x / 2;
+        int ans = 0;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (mid <= x / mid) {   // avoids overflow
+                ans = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
