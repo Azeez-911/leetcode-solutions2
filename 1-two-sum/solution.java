e29719996c33a2418fb068f35eeb6e86ef6e4cfe
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-06-24 19:48:33 +0530

    🟢 Two Sum [Easy] - java

---
+++ 1-two-sum/solution.java
@@ -0,0 +1,30 @@
+/*
+ * LeetCode #1 - Two Sum
+ * Difficulty : Easy
+ * Language   : java
+ * Runtime    : 2 ms
+ * Memory     : 46.75 MB
+ * URL        : https://leetcode.com/problems/two-sum/
+ */
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if(map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
