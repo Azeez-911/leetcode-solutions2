f09c8190af8598f8ed8f44910d471288faebf74e
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-06-24 20:05:36 +0530

    🟢 Remove Element [Easy] - java

---
+++ 27-remove-element/solution.java
@@ -0,0 +1,14 @@
+/*
+ * LeetCode #27 - Remove Element
+ * Difficulty : Easy
+ * Language   : java
+ * Runtime    : 0 ms
+ * Memory     : 10.36 MB
+ * URL        : https://leetcode.com/problems/remove-element/
+ */
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        
+    }
+}
