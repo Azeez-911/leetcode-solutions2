abf0db53d31f6032e7d78870534166e58c43db27
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-06-25 10:01:31 +0530

    🟡 Add Two Numbers [Medium] - java

--- 2-add-two-numbers/solution.java
+++ 2-add-two-numbers/solution.java
@@ -3,30 +3,38 @@
  * Difficulty : Medium
  * Language   : java
  * Runtime    : 1 ms
- * Memory     : 46.68 MB
+ * Memory     : 46.03 MB
  * URL        : https://leetcode.com/problems/add-two-numbers/
  */
 
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { 
- *         this.val = val; 
- *         this.next = next; 
- *     }
- * }
+ *     struct ListNode *next;
+ * };
  */
-
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        
-        int carry = 0;
-        
-  
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode* l3 = NULL, *p1 = l1, *p2 = l2, *p3 = l3, *p3_prev = NULL; int carry = 0; int turn = 0;
+  while(p1 || p2 || carry) {
+    int val1 = 0, val2 = 0, val3 = 0;
+    if (p1) {
+      val1 = p1->val;
+      p1 = p1->next;
+    }
+    if (p2) {
+      val2 = p2->val;
+      p2 = p2->next;
+    }
+    val3 = val1 + val2 + carry;
+    carry = val3 / 10;
+    p3 = (struct ListNode *) malloc(sizeof(struct ListNode)); p3->val = val3 % 10; p3->next = NULL;
+    if (!turn)
+      l3 = p3;
+    else
+      p3_prev->next = p3;
+    p3_prev = p3;
+    turn++;
+  }
+  return l3;
+}
