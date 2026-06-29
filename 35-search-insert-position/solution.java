9f7a0cf98bcddbf1d2b50119535cfdc4abcf3bd0
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-06-29 23:19:35 +0530

    🟢 Search Insert Position [Easy] - java

---
+++ 35-search-insert-position/solution.java
@@ -0,0 +1,29 @@
+/*
+ * LeetCode #35 - Search Insert Position
+ * Difficulty : Easy
+ * Language   : java
+ * Runtime    : 0 ms
+ * Memory     : 44.64 MB
+ * URL        : https://leetcode.com/problems/search-insert-position/
+ */
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
+}
