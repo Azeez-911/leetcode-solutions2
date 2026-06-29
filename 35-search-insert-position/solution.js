588785f5e473862d54a07b8264f5fc05b37f5d04
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-06-29 23:19:18 +0530

    🟢 Search Insert Position [Easy] - javascript

---
+++ 35-search-insert-position/solution.js
@@ -0,0 +1,14 @@
+/*
+ * LeetCode #35 - Search Insert Position
+ * Difficulty : Easy
+ * Language   : javascript
+ * Runtime    : N/A
+ * Memory     : N/A
+ * URL        : https://leetcode.com/problems/search-insert-position/
+ */
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        
+    }
+}
