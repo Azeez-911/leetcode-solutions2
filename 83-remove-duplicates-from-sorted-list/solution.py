b6066004d1e163b62f91a3fda4c83ea4fbb92bf2
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-07-02 22:32:32 +0530

    🟢 Remove Duplicates from Sorted List [Easy] - python

---
+++ 83-remove-duplicates-from-sorted-list/solution.py
@@ -0,0 +1,24 @@
+# LeetCode #83 - Remove Duplicates from Sorted List
+# Difficulty : Easy
+# Language   : python
+# Runtime    : 3 ms
+# Memory     : 12.30 MB
+# URL        : https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head):
+        current = head
+
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
