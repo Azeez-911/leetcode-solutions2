a4e05d51b134cefb41845332da9243c613dd246c
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-07-07 18:53:57 +0530

    🟢 Palindrome Number [Easy] - java

---
+++ 0009-palindrome-number/solution.java
@@ -0,0 +1,26 @@
+/*
+ * LeetCode #9 - Palindrome Number
+ * Difficulty : Easy
+ * Language   : java
+ * Runtime    : 3 ms
+ * Memory     : 8.49 MB
+ * URL        : https://leetcode.com/problems/palindrome-number/
+ */
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int original = x;
+        long long reversed = 0;
+
+        while (x > 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+};
