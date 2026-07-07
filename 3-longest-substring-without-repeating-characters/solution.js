9c959086f9bb1d13ea3ceef88c51d9854b722820
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-07-07 17:11:31 +0530

    🟡 Longest Substring Without Repeating Characters [Medium] - javascript

---
+++ 3-longest-substring-without-repeating-characters/solution.js
@@ -0,0 +1,16 @@
+/*
+ * LeetCode #3 - Longest Substring Without Repeating Characters
+ * Difficulty : Medium
+ * Language   : javascript
+ * Runtime    : 2 ms
+ * Memory     : 58.27 MB
+ * URL        : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    
+};
