07bfc201699b07690cdf7a4e56335915ad07c37f
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-07-07 17:03:40 +0530

    🟡 Longest Substring Without Repeating Characters [Medium] - python

---
+++ 3-longest-substring-without-repeating-characters/solution.py
@@ -0,0 +1,21 @@
+# LeetCode #3 - Longest Substring Without Repeating Characters
+# Difficulty : Medium
+# Language   : python
+# Runtime    : 15 ms
+# Memory     : 12.54 MB
+# URL        : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        last_seen = {}
+        left = 0
+        max_len = 0
+
+        for right, ch in enumerate(s):
+            if ch in last_seen and last_seen[ch] >= left:
+                left = last_seen[ch] + 1
+
+            last_seen[ch] = right
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
