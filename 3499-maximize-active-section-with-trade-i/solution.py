d05fd4a8c175ca13e2309f610b8d5822a0edba18
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-07-22 15:00:16 +0530

    🟡 Maximize Active Section with Trade I [Medium] - python

---
+++ 3499-maximize-active-section-with-trade-i/solution.py
@@ -0,0 +1,31 @@
+# LeetCode #3499 - Maximize Active Section with Trade I
+# Difficulty : Medium
+# Language   : python
+# Runtime    : 731 ms
+# Memory     : 20.68 MB
+# URL        : https://leetcode.com/problems/maximize-active-section-with-trade-i/
+
+class Solution:
+    def maxActiveSectionsAfterTrade(self, s: str) -> int:
+        ans = 0
+        pre = float('-inf')
+        mx = 0
+        i = 0
+        n = len(s)
+
+        while i < n:
+            j = i
+            while j < n and s[j] == s[i]:
+                j += 1
+
+            cur = j - i
+
+            if s[i] == '1':
+                ans += cur
+            else:
+                mx = max(mx, pre + cur)
+                pre = cur
+
+            i = j
+
+        return ans + mx
