f37ba7c48fe2f5f5779b8daaf0a7e67ac3d25a12
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-07-24 14:56:47 +0530

    🟡 Reverse Integer [Medium] - python

--- 0007-reverse-integer/solution.py
+++ 0007-reverse-integer/solution.py
@@ -1,25 +1,16 @@
 # LeetCode #7 - Reverse Integer
 # Difficulty : Medium
 # Language   : python
-# Runtime    : 49 ms
-# Memory     : 19.22 MB
+# Runtime    : 41 ms
+# Memory     : 19.14 MB
 # URL        : https://leetcode.com/problems/reverse-integer/
 
 class Solution:
     def reverse(self, x: int) -> int:
 
         sign = -1 if x < 0 else 1
-        x = abs(x)
 
-        reversed_num = 0
-
-        while x:
-
-            digit = x % 10
-            reversed_num = reversed_num * 10 + digit
-            x //= 10
-
-        reversed_num *= sign
+        reversed_num = int(str(abs(x))[::-1]) * sign
 
         if reversed_num < -2**31 or reversed_num > 2**31 - 1:
             return 0
