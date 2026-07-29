a65b8b51fcfdb02b9bac6c4447257be3010fceca
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-07-29 22:26:45 +0530

    🟢 Add Digits [Easy] - java

---
+++ 0258-add-digits/solution.java
@@ -0,0 +1,25 @@
+/*
+ * LeetCode #258 - Add Digits
+ * Difficulty : Easy
+ * Language   : java
+ * Runtime    : 1 ms
+ * Memory     : 42.86 MB
+ * URL        : https://leetcode.com/problems/add-digits/
+ */
+
+class Solution {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            num = sum;
+        }
+
+        return num;
+    }
+}
