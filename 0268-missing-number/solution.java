e2a802df1fab20d959fe8490266a4598b97fe3ef
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-08-05 09:05:11 +0530

    🟢 Missing Number [Easy] - java

--- 0268-missing-number/solution.java
+++ 0268-missing-number/solution.java
@@ -2,29 +2,29 @@
  * LeetCode #268 - Missing Number
  * Difficulty : Easy
  * Language   : java
- * Runtime    : 8 ms
- * Memory     : 47.95 MB
+ * Runtime    : 0 ms
+ * Memory     : 47.34 MB
  * URL        : https://leetcode.com/problems/missing-number/
  */
 
 class Solution {
     public int missingNumber(int[] nums) {
-        // int l = nums.length;
-        // int sum=0;
-        // for(int i=0;i<l;i++){
-        //     sum+=nums[i];
-        // }
-        // int r = (l*(l+1))/2-sum;
-        // return r;
-        Arrays.sort(nums);
-        int i = 0;
-        for (int num : nums){
-            if(num != i){
-                return i;
-            }
-            i++;
+        int l = nums.length;
+        int sum=0;
+        for(int i=0;i<l;i++){
+            sum+=nums[i];
         }
+        int r = (l*(l+1))/2-sum;
+        return r;
+        // Arrays.sort(nums);
+        // int i = 0;
+        // for (int num : nums){
+        //     if(num != i){
+        //         return i;
+        //     }
+        //     i++;
+        // }
 
-        return nums.length;
+        // return nums.length;
     }
 }
