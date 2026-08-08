656e727cc5e47c7c6b9fb4b4d2a24b7f24c78e86
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-08-08 11:25:08 +0530

    🟢 Smallest Even Multiple [Easy] - java

---
+++ 2413-smallest-even-multiple/solution.java
@@ -0,0 +1,18 @@
+/*
+ * LeetCode #2413 - Smallest Even Multiple
+ * Difficulty : Easy
+ * Language   : java
+ * Runtime    : 0 ms
+ * Memory     : 0.00 MB
+ * URL        : https://leetcode.com/problems/smallest-even-multiple/
+ */
+
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) {
+            return n;
+        }
+
+        return n * 2;
+    }
+}
