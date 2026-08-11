b0e263b25291216d7a8229d3c966d6b70a84df73
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-08-11 10:29:36 +0530

    🟢 Smallest Missing Integer Greater Than Sequential Prefix Sum [Easy] - java

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/solution.java
@@ -0,0 +1,28 @@
+/*
+ * LeetCode #2996 - Smallest Missing Integer Greater Than Sequential Prefix Sum
+ * Difficulty : Easy
+ * Language   : java
+ * Runtime    : 1 ms
+ * Memory     : 44.17 MB
+ * URL        : https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/
+ */
+
+class Solution:
+    def missingInteger(self, nums: List[int]) -> int:
+        
+        tot = nums[0]
+        found = False
+        for i, num in enumerate(nums):
+            next = i+1
+            
+            if not found:
+                if next < len(nums) and nums[next] == num + 1:
+                    tot += nums[next]
+                else:
+                    found = True
+
+        ans = tot
+        while ans in nums:
+            ans += 1
+        
+        return ans
