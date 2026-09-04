4c8e1f02bbb01dfd8ca7df4884c796d31e83dad9
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-09-04 21:03:47 +0530

    🟢 Smallest Stable Index I [Easy] - java

---
+++ 3903-smallest-stable-index-i/solution.java
@@ -0,0 +1,37 @@
+/*
+ * LeetCode #3903 - Smallest Stable Index I
+ * Difficulty : Easy
+ * Language   : java
+ * Runtime    : 0 ms
+ * Memory     : 0.00 MB
+ * URL        : https://leetcode.com/problems/smallest-stable-index-i/
+ */
+
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int max = nums[0];
+
+            // Find max from 0 to i
+            for (int j = 0; j <= i; j++) {
+                max = Math.max(max, nums[j]);
+            }
+
+            int min = nums[i];
+
+            // Find min from i to n-1
+            for (int j = i; j < n; j++) {
+                min = Math.min(min, nums[j]);
+            }
+
+            if (max - min <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
