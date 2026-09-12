d30d2f15c5dc7eedae94636445dcf56d5cd66c80
Azeez-911 <sk.abdulazeez777@gmail.com>
2026-09-12 10:18:47 +0530

    🟡 Count and Say [Medium] - java

---
+++ 0038-count-and-say/solution.java
@@ -0,0 +1,41 @@
+/*
+ * LeetCode #38 - Count and Say
+ * Difficulty : Medium
+ * Language   : java
+ * Runtime    : 8 ms
+ * Memory     : 42.40 MB
+ * URL        : https://leetcode.com/problems/count-and-say/
+ */
+
+class Solution {
+    public String countAndSay(int n) {
+
+        String s = "1";
+
+        for (int i = 1; i < n; i++) {
+
+            StringBuilder next = new StringBuilder();
+
+            int count = 1;
+
+            for (int j = 1; j < s.length(); j++) {
+
+                if (s.charAt(j) == s.charAt(j - 1)) {
+                    count++;
+                } else {
+                    next.append(count);
+                    next.append(s.charAt(j - 1));
+                    count = 1;
+                }
+            }
+
+            // Add the last group
+            next.append(count);
+            next.append(s.charAt(s.length() - 1));
+
+            s = next.toString();
+        }
+
+        return s;
+    }
+}
